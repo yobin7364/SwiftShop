5ffb90b2e85907d74be8705039250c1394b0d5af
Yobin Kumar Pun <punyobin123@gmail.com>
2025-04-04 15:48:08 +1100

    completed user landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import HomePage from "./components/pages/buyer/HomePage/HomePage";
+import NavBar from "./components/common/NavBar";
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
